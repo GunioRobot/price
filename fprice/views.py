5ffefb57e20053107847ed3079ea3f8e8dabe45d
petrof <petrof.alexander@gmail.com>
2011-01-05 14:51:11 +0300

    fix - shop wiwithout address

--- fprice/views.py
+++ fprice/views.py
@@ -135,33 +135,35 @@ def shop_info(request, shop_id):
     location = 0
     if not shop:
         shop = Shop.objects.get(pk=1)
-    if (not shop.addr.x or not shop.addr.y):
-        ip = request.META['REMOTE_ADDR']
+    if shop.addr:
+        if (not shop.addr.x or not shop.addr.y):
+            ip = request.META['REMOTE_ADDR']
 
-        # Посылка запроса на ipgeobase.ru
-        conn = httplib.HTTPConnection("194.85.91.253:8090")
-        conn.request("POST", "/geo/geo.html",\
-                    "<ipquery><fields><all/></fields><ip-list><ip>" + ip +\
-                    "</ip></ip-list></ipquery>")
-        resp = conn.getresponse()
+            # Посылка запроса на ipgeobase.ru
+            conn = httplib.HTTPConnection("194.85.91.253:8090")
+            conn.request("POST", "/geo/geo.html",\
+                        "<ipquery><fields><all/></fields><ip-list><ip>" + ip +\
+                        "</ip></ip-list></ipquery>")
+            resp = conn.getresponse()
 
-        data = resp.read()
-        conn.close()
+            data = resp.read()
+            conn.close()
 
-        # если ничего не найдено
-        if re.search("<message>Not found</message>", data):
-            location = 2 # адрес нулевой
+            # если ничего не найдено
+            if re.search("<message>Not found</message>", data):
+                location = 2 # адрес нулевой
+            else:
+                dom = parseString(data)
+                # http://blog.ipgeobase.ru/?p=37 - теги расписаны тут
+                lat_y = dom.documentElement.getElementsByTagName('lat')[0].firstChild.nodeValue
+                lon_x = dom.documentElement.getElementsByTagName('lng')[0].firstChild.nodeValue
+                location = 1 # есть адрес города
         else:
-            dom = parseString(data)
-            # http://blog.ipgeobase.ru/?p=37 - теги расписаны тут
-            lat_y = dom.documentElement.getElementsByTagName('lat')[0].firstChild.nodeValue
-            lon_x = dom.documentElement.getElementsByTagName('lng')[0].firstChild.nodeValue
-            location = 1 # есть адрес города
+            lon_x = shop.addr.x
+            lat_y = shop.addr.y
+            zoom = 16
     else:
-        lon_x = shop.addr.x
-        lat_y = shop.addr.y
-        zoom = 16
-        
+        location = 2
 
     return render_to_response('shop_info.html',
         {'shop': shop, 'lat_y': lat_y, 'lon_x': lon_x, 'zoom': zoom, 'location': location},
