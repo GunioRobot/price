187f4ffe4609f6a703b791f0e558b8d299f30f5c
dfalk <tfalc@mail.ru>
2011-01-21 11:20:11 +0300

    price app: convert to south

---
+++ price/migrations/0001_initial.py
@@ -0,0 +1,165 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'Section'
+        db.create_table('price_section', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=50)),
+        ))
+        db.send_create_signal('price', ['Section'])
+
+        # Adding model 'Goods'
+        db.create_table('price_goods', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=100)),
+            ('ed', self.gf('django.db.models.fields.CharField')(max_length=5)),
+            ('section', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['price.Section'], null=True, blank=True)),
+            ('descr', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
+        ))
+        db.send_create_signal('price', ['Goods'])
+
+        # Adding model 'Trade'
+        db.create_table('price_trade', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('shop', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['shop.Shop'])),
+            ('goods', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['price.Goods'])),
+            ('time', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('time_add', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('amount', self.gf('django.db.models.fields.FloatField')()),
+            ('price', self.gf('django.db.models.fields.DecimalField')(max_digits=19, decimal_places=2)),
+            ('currency', self.gf('django.db.models.fields.CharField')(max_length=3)),
+            ('cost', self.gf('django.db.models.fields.DecimalField')(max_digits=12, decimal_places=2)),
+            ('spytrade', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal('price', ['Trade'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'Section'
+        db.delete_table('price_section')
+
+        # Deleting model 'Goods'
+        db.delete_table('price_goods')
+
+        # Deleting model 'Trade'
+        db.delete_table('price_trade')
+
+
+    models = {
+        'address.address': {
+            'Meta': {'object_name': 'Address'},
+            'city': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['address.City']"}),
+            'house': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'housing': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'office': ('django.db.models.fields.CharField', [], {'max_length': '10', 'null': 'True', 'blank': 'True'}),
+            'street': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['address.Street']"}),
+            'x': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'}),
+            'y': ('django.db.models.fields.FloatField', [], {'null': 'True', 'blank': 'True'})
+        },
+        'address.city': {
+            'Meta': {'ordering': "['title']", 'object_name': 'City'},
+            'country': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['address.Country']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'address.country': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Country'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'address.street': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Street'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '80', 'unique': 'True'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'max_length': '30', 'unique': 'True'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'price.goods': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Goods'},
+            'descr': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'ed': ('django.db.models.fields.CharField', [], {'max_length': '5'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'section': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['price.Section']", 'null': 'True', 'blank': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'price.section': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Section'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'price.trade': {
+            'Meta': {'ordering': "['-time']", 'object_name': 'Trade'},
+            'amount': ('django.db.models.fields.FloatField', [], {}),
+            'cost': ('django.db.models.fields.DecimalField', [], {'max_digits': '12', 'decimal_places': '2'}),
+            'currency': ('django.db.models.fields.CharField', [], {'max_length': '3'}),
+            'goods': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['price.Goods']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'price': ('django.db.models.fields.DecimalField', [], {'max_digits': '19', 'decimal_places': '2'}),
+            'shop': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['shop.Shop']"}),
+            'spytrade': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'time': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'time_add': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"})
+        },
+        'shop.center': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Center'},
+            'addr': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['address.Address']"}),
+            'descr': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'shop.shop': {
+            'Meta': {'ordering': "['title']", 'object_name': 'Shop'},
+            'addr': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['address.Address']", 'null': 'True', 'blank': 'True'}),
+            'center': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['shop.Center']", 'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '50'}),
+            'type': ('django.db.models.fields.CharField', [], {'max_length': '3'})
+        }
+    }
+
+    complete_apps = ['price']
